1671c96280bff04d1efdcd2961e6940591d7058d
Felipe Rubio <juan.feliperubiog@gmail.com>
2020-09-13 03:26:23 -0500

    HealthBar
    Se me olvido...

--- Assets/Scripts/Critter/Health.cs
+++ Assets/Scripts/Critter/Health.cs
@@ -9,26 +9,23 @@ public class Health : MonoBehaviour
     private Affinity critterAf;
     public float currentHealth;
     private SpriteRenderer critterSprite;
+    [SerializeField]
+    private RectTransform healtBar;
+    private Vector2 initialHealBarSize;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        initialHealBarSize = healtBar.sizeDelta;
         critter = GetComponent<Critter>();
         critterAf = critter.Affinity;
         currentHealth = critter.BaseHp;
         critterSprite = GetComponent<SpriteRenderer>();
     }
-    public void TakeDamage(float amout, Affinity atkType)
+    public void TakeDamage(float amount, Affinity atkType)
     {
-        currentHealth -= amout * AffinityMultiplier.Multiplier(critterAf, atkType);
-        critterSprite.color = Color.red;
-        StartCoroutine(RestoreColor());
-        if (currentHealth <= 0)
-        {
-            ImDead();
-        }
-        
+        StartCoroutine(DamageEffect(amount,atkType));
     }
 
     private void ImDead()
@@ -36,10 +33,27 @@ public class Health : MonoBehaviour
         critter.Owner.RemoveCritter(critter);
     }
 
-    private IEnumerator RestoreColor()
+    private IEnumerator DamageEffect(float amount, Affinity atkType)
     {
-        yield return new WaitForSeconds(0.5f);
+        float time, dmgTime;
+        time = 0;
+        dmgTime = 0.5f;
+        critterSprite.color = Color.red;
+        Vector3 spritePos = transform.position;
+        while (time <= dmgTime)
+        {
+            time += Time.deltaTime;
+            transform.position += new Vector3( Random.Range(-0.1f,0.1f), Random.Range(-0.1f, 0.1f), 0);
+            yield return new WaitForEndOfFrame();
+        }
+        transform.position = spritePos;
         critterSprite.color = Color.white;
+        currentHealth -= amount * AffinityMultiplier.Multiplier(critterAf, atkType);
+        healtBar.sizeDelta = new Vector2(currentHealth*initialHealBarSize.x/critter.BaseHp, initialHealBarSize.y);
+        if (currentHealth <= 0)
+        {
+            ImDead();
+        }
     }
 }
 
